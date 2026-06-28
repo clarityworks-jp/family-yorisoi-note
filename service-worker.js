beb2868eabe7c68c8720b4f213989811c4cefc6d
clarityworks-jp <clarityworks-jp@users.noreply.github.com>
2026-06-28 09:13:08 +0900

    Prefer fresh app shell assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yorisoi-note-v11";
+const CACHE_NAME = "yorisoi-note-v12";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -43,12 +43,12 @@ self.addEventListener("fetch", (event) => {
   }
 
   event.respondWith(
-    caches.match(event.request).then((cached) => (
-      cached || fetch(event.request).then((response) => {
+    fetch(event.request)
+      .then((response) => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
       })
-    ))
+      .catch(() => caches.match(event.request))
   );
 });
